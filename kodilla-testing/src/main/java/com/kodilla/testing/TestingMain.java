3be53e124da225ae85bc699df4ccc21e0d8297bd
Oskar Sęk <oskar.sek@itpendent.com>
2022-05-10 20:15:31 +0200

    Add Calculator.java, SimpleUser.java and Tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,43 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        simpleUserTest();
+
+        calculatorTest();
+    }
+
+    private static void simpleUserTest() {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+        printTestStatus(result.equals("theForumUser"));
+    }
+
+    private static void calculatorTest() {
+        System.out.println("Test - pierwszy test jednostkowy:");
+        calculatorAddTest();
+        System.out.println("Test - drugi test jednostkowy:");
+        calculatorSubtractTest();
+    }
+
+    private static void calculatorSubtractTest() {
+        printTestStatus(Calculator.subtract(5, 3) == 2);
+    }
+
+    private static void calculatorAddTest() {
+        printTestStatus(Calculator.add(5, 3) == 8);
+    }
+
+
+    private static void printTestStatus(boolean res) {
+        if (res) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.calculator;
+
+abstract public class Calculator {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+}
