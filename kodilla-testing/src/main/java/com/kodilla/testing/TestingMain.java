b09fcb31913a298fc1710898bd2004ebef345da0
Oskar Sęk <oskar.sek@itpendent.com>
2022-05-14 21:55:31 +0200

    Exercise with 6.4 JUnit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,39 +5,5 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
-        simpleUserTest();
-
-        calculatorTest();
-    }
-
-    private static void simpleUserTest() {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-        printTestStatus(result.equals("theForumUser"));
-    }
-
-    private static void calculatorTest() {
-        System.out.println("Test - pierwszy test jednostkowy:");
-        calculatorAddTest();
-        System.out.println("Test - drugi test jednostkowy:");
-        calculatorSubtractTest();
-    }
-
-    private static void calculatorSubtractTest() {
-        printTestStatus(Calculator.subtract(5, 3) == 2);
-    }
-
-    private static void calculatorAddTest() {
-        printTestStatus(Calculator.add(5, 3) == 8);
-    }
-
-
-    private static void printTestStatus(boolean res) {
-        if (res) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
     }
 }
