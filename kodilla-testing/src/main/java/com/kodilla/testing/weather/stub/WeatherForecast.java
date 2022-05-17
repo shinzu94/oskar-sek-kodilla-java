58b24e1095bfed9c40eb16c2c351286893c9e9c4
Oskar Sęk <oskar.sek@itpendent.com>
2022-05-17 23:33:15 +0200

    Task from 6.5 Mockito

--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -2,9 +2,10 @@ package com.kodilla.testing.weather.stub;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.OptionalDouble;
 
 public class WeatherForecast {
-    private Temperatures temperatures;
+    private final Temperatures temperatures;
 
     public WeatherForecast(Temperatures temperatures) {
         this.temperatures = temperatures;
@@ -21,5 +22,26 @@ public class WeatherForecast {
             resultMap.put(temperature.getKey(), temperature.getValue() + 1.0); // [1]
         }
         return resultMap;
+    }
+
+    public OptionalDouble calculateAverageTemperatureForecast() {
+        Map<String, Double> map = calculateForecast();
+        return map.values().stream().mapToDouble(v -> v).average();
+    }
+
+    public OptionalDouble calculateMedianTemperatureForecast() {
+        Map<String, Double> map = calculateForecast();
+        int size = map.size();
+        if (size > 0) {
+            double[] array = map.values().stream().mapToDouble(v -> v).sorted().toArray();
+
+            int halfSize = size / 2;
+            if (size % 2 == 1) {
+                return OptionalDouble.of(array[halfSize]);
+            } else {
+                return OptionalDouble.of(((array[halfSize - 1] + array[halfSize]) / 2));
+            }
+        }
+        return OptionalDouble.empty();
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/weather/stub/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/stub/WeatherForecastTestSuite.java
@@ -1,38 +1,119 @@
 package com.kodilla.testing.weather.stub;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.OptionalDouble;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class WeatherForecastTestSuite {
-
+    private static final Map<String, Double> TEMPERATURE_EMPTY_MAP = Collections.unmodifiableMap(new HashMap<>());
+    private static Map<String, Double> exampleTemperatureMap;
     @Mock
     private Temperatures temperaturesMock;
+
+    @BeforeAll
+    static void beforeAll() {
+        exampleTemperatureMap = new HashMap<>();
+        exampleTemperatureMap.put("Rzeszow", 25.5);
+        exampleTemperatureMap.put("Krakow", 26.2);
+        exampleTemperatureMap.put("Wroclaw", 24.8);
+        exampleTemperatureMap.put("Warszawa", 25.2);
+        exampleTemperatureMap.put("Gdansk", 26.1);
+    }
 
     @Test
     void testCalculateForecastWithMock() {
         //Given
-        Map<String, Double> temperaturesMap = new HashMap<>();
-        temperaturesMap.put("Rzeszow", 25.5);
-        temperaturesMap.put("Krakow", 26.2);
-        temperaturesMap.put("Wroclaw", 24.8);
-        temperaturesMap.put("Warszawa", 25.2);
-        temperaturesMap.put("Gdansk", 26.1);
-        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
         WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(exampleTemperatureMap);
 
         //When
         int quantityOfSensors = weatherForecast.calculateForecast().size();
 
         //Then
-        Assertions.assertEquals(5, quantityOfSensors);
+        assertEquals(5, quantityOfSensors);
+    }
+
+    @Test
+    void testCalculateAverageTemperatureForEmptyForecast() {
+        //Given
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(TEMPERATURE_EMPTY_MAP);
+
+        //When
+        OptionalDouble result = weatherForecast.calculateAverageTemperatureForecast();
+
+        //Then
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    void testCalculateAverageTemperatureForecast() {
+        //Given
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(exampleTemperatureMap);
+
+        //When
+        OptionalDouble result = weatherForecast.calculateAverageTemperatureForecast();
+
+        //Then
+        assertTrue(result.isPresent());
+        assertEquals(26.56, Math.round(result.getAsDouble() * 100) / 100D);
+    }
+
+    @Test
+    void testCalculateMedianTemperatureForEmptyForecast() {
+        //Given
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(TEMPERATURE_EMPTY_MAP);
+
+        //When
+        OptionalDouble result = weatherForecast.calculateMedianTemperatureForecast();
+
+        //Then
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    void testCalculateMedianTemperatureForecast() {
+        //Given
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(exampleTemperatureMap);
+
+        //When
+        OptionalDouble result = weatherForecast.calculateMedianTemperatureForecast();
+
+        //Then
+        assertTrue(result.isPresent());
+        assertEquals(26.5, Math.round(result.getAsDouble() * 100) / 100D);
+    }
+
+    @Test
+    void testCalculateMedianTemperatureForecastWithEvenLength() {
+        //Given
+        Map<String, Double> temperatureMapWithEvenLength = new HashMap<>();
+        temperatureMapWithEvenLength.put("Rzeszow", 25.5);
+        temperatureMapWithEvenLength.put("Krakow", 26.2);
+        temperatureMapWithEvenLength.put("Wroclaw", 24.8);
+        temperatureMapWithEvenLength.put("Warszawa", 25.2);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperatureMapWithEvenLength);
+
+        //When
+        OptionalDouble result = weatherForecast.calculateMedianTemperatureForecast();
+
+        //Then
+        assertTrue(result.isPresent());
+        assertEquals(26.35, Math.round(result.getAsDouble() * 100) / 100D);
     }
 }
