1d9fa4fbd4f58579e365f6160180ad77bcb7ca7a
Oskar Sęk <oskar.sek@itpendent.com>
2022-05-19 18:50:33 +0200

    Exercise with 7.2 Lambda

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,8 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-
-import javax.swing.*;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -10,5 +9,7 @@ public class StreamMain {
         System.out.println(PoemBeautifier.beautify("test", string -> "abc" + string.toUpperCase()));
         System.out.println(PoemBeautifier.beautify("test", string -> string.repeat(3)));
         System.out.println(PoemBeautifier.beautify(" test4 ", string -> string.trim()));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
